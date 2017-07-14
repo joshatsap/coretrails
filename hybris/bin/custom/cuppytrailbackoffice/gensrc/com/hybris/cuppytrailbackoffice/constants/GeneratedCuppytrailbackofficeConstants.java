402d5caa610291411f8a446b51179df460bf1136
Joshua Doyle | Hybris <joshua.doyle@sap.com>
2017-07-14 14:49:30 -0400

    added backoffice

---
+++ hybris/bin/custom/cuppytrailbackoffice/gensrc/com/hybris/cuppytrailbackoffice/constants/GeneratedCuppytrailbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 13-Jul-2017 10:23:45 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.hybris.cuppytrailbackoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCuppytrailbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "cuppytrailbackoffice";
+	
+	protected GeneratedCuppytrailbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
